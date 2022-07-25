28696c92815be3f27793193fbae951a861145f5f
Viktor Rosenfeld <viktor.rosenfeld@tu-berlin.de>
2022-07-25 16:56:51 +0200

    Update Java code to new Java API

--- java-client-example/src/main/java/NebulaStreamTutorial.java
+++ java-client-example/src/main/java/NebulaStreamTutorial.java
@@ -1,32 +1,35 @@
-import stream.nebula.API.Query;
-import stream.nebula.exceptions.EmptyFieldException;
 import stream.nebula.exceptions.RESTException;
 import stream.nebula.operators.Aggregation;
-import stream.nebula.operators.EventTime;
-import stream.nebula.operators.TimeMeasure;
-import stream.nebula.operators.sink.FileSink;
-import stream.nebula.operators.windowdefinition.TumblingWindow;
+import stream.nebula.operators.sinks.FileSink;
+import stream.nebula.operators.sinks.Sink;
+import stream.nebula.operators.window.TumblingWindow;
 import stream.nebula.runtime.NebulaStreamRuntime;
+import stream.nebula.runtime.Query;
 
 import java.io.IOException;
 
+import static stream.nebula.operators.window.EventTime.eventTime;
+import static stream.nebula.operators.window.TimeMeasure.minutes;
+
 public class NebulaStreamTutorial {
 
-    public static void main(String[] args) throws EmptyFieldException, RESTException, IOException {
+    public static void main(String[] args) throws RESTException, IOException {
         // Create a NebulaStream runtime and connect it to the NebulaStream coordinator.
         NebulaStreamRuntime nebulaStreamRuntime = NebulaStreamRuntime.getRuntime();
         nebulaStreamRuntime.getConfig().setHost("localhost").setPort("8081");
 
         // Create a streaming query
-        Query query = new Query()
-                .from("wind_turbines")
-                .window(TumblingWindow.of(new EventTime("features_properties_updated"), TimeMeasure.minutes(10)))
+        Query query = nebulaStreamRuntime
+                .readFromSource("wind_turbines")
+                .window(TumblingWindow.of(eventTime("features_properties_updated"), minutes(10)))
                 .byKey("metadata_id")
-                .apply(Aggregation.sum("features_properties_mag"))
-                .sink(new FileSink("/tutorial/java-query-results.csv", "CSV_FORMAT", "OVERRIDE"));
+                .apply(Aggregation.sum("features_properties_mag"));
+
+        // Finish the query with a sink
+        Sink sink = query.sink(new FileSink("/tutorial/java-query-results.csv", "CSV_FORMAT", "OVERRIDE"));
 
         // Submit the query to the coordinator.
-        int queryId = nebulaStreamRuntime.executeQuery(query.generateCppCode(), "BottomUp");
+        int queryId = nebulaStreamRuntime.executeQuery(query, "BottomUp");
     }
 
 }
