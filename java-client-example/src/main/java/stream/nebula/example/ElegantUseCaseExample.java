1790648a40fa0c7fb1f4c0399d0c7f73feefd28e
Viktor Rosenfeld <viktor.rosenfeld@tu-berlin.de>
2023-08-13 23:11:51 +0200

    [#24] Initial ELEGANT deployment example

--- java-client-example/src/main/java/stream/nebula/example/ElegantUseCaseExample.java
+++ java-client-example/src/main/java/stream/nebula/example/ElegantUseCaseExample.java
@@ -1,25 +1,20 @@
 package stream.nebula.example;
 
 import stream.nebula.exceptions.RESTException;
-import stream.nebula.operators.Aggregation;
 import stream.nebula.operators.sinks.FileSink;
 import stream.nebula.operators.sinks.Sink;
-import stream.nebula.operators.window.TumblingWindow;
 import stream.nebula.runtime.NebulaStreamRuntime;
 import stream.nebula.runtime.Query;
-import stream.nebula.serialization.cpp.CppQueryRequestSerializer;
 import stream.nebula.udf.MapFunction;
 
 import java.io.IOException;
 
 import static stream.nebula.expression.Expressions.attribute;
-import static stream.nebula.operators.window.EventTime.eventTime;
-import static stream.nebula.operators.window.TimeMeasure.minutes;
 
 /**
  * Example demonstrating the execution of a Java UDF.
  */
-public class JavaUdfExample {
+public class ElegantUseCaseExample {
 
     // This is the input type of the UDF.
     // The names of the fields `x` and `y` must correspond to the schema of the input stream.
@@ -50,7 +45,7 @@ public class JavaUdfExample {
         nebulaStreamRuntime.getConfig().setHost("localhost").setPort("8081");
 
         // Create a streaming query. The `points` source contains two attributes `x` and `y`.
-        Query query = nebulaStreamRuntime.readFromSource("points");
+        Query query = nebulaStreamRuntime.readFromSource("cartesian-points");
 
         // Project the input attributes for the UDF. This operation is not strictly necessary in this example,
         // because the input stream only contains the two attributes.
@@ -60,10 +55,10 @@ public class JavaUdfExample {
         query.map(new CartesianToPolar());
 
         // Finish the query with a sink
-        Sink sink = query.sink(new FileSink("/tutorial/output/java-udf-results.csv", "CSV_FORMAT", true));
+        Sink sink = query.sink(new FileSink("/tutorial/output/polar-points.csv", "CSV_FORMAT", true));
 
         // Submit the query to the coordinator.
-        nebulaStreamRuntime.executeQuery(query, "BottomUp");
+        nebulaStreamRuntime.executeQuery(query, "ELEGANT-PERFORMANCE");
     }
 
 }
